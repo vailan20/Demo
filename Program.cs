00a5dcb0c30a2bad5f983c0aabc8dca84560bdac
Vailantina Fernandes <fernandesvailantina@gmail.com>
2021-07-29 13:50:50 +0530

    demo project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
